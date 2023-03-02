6cfde20a3d785b84f266f0a6983c400f5ff505d7
git_LinexMugambi <linexmugambi254@gmail.com>
2023-03-02 13:43:52 +0300

    updated and styled

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,19 +1,20 @@
 import React from "react";
+import "./App/Navbar.css"
 
 export default function Navbar() {
   return (
     <div className="Navbar">
-  <nav className="navbar navbar-expand-lg ">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="Home">Home</a>
-      <a className="navbar-brand" href="add" >add</a>
-      <a className="navbar-brand" href="logout">logout</a>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="Home">Home</a>
+          <a className="navbar-brand" href="add">Add</a>
+          <a className="navbar-brand" href="logout">Logout</a>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
     </div>
-  </nav>
-</div>
   );
 }
