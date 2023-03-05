f40ebc8b5223955dab2f0f8e531d3d1f7943914a
git_LinexMugambi <linexmugambi254@gmail.com>
2023-03-05 12:39:18 +0300

    updated pets.js

--- client/src/components/Pets/pets.js
+++ client/src/components/Pets/pets.js
@@ -2,42 +2,45 @@ import axios from "axios";
 import DisplayPets from "./displaypets";
 import React, { useState, useEffect } from "react";
 
-const URL = "https://pet-finder-9j4w.onrender.com/pets";
+const URL = "https://pet-finder-pgl9.onrender.com/pets";
 
 export default function Pets() {
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
-    axios.get(URL).then((resp) => {
-      setPets(resp.data);
-    });
+    axios
+      .get(URL)
+      .then((resp) => {
+        setPets(resp.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, []);
 
   return (
     <div>
-<h1>Animal Haven</h1>
-    <div className="pets">
-      {pets.map((newpets)=>{
-        console.log(newpets);
-        return(
-          <div key={newpets.id}>
-            <div>
-              <DisplayPets
-              name = {newpets.name}
-              age = {newpets.age}
-              species = {newpets.species}
-              breed = {newpets.breed}
-              image = {newpets.image}
-              gender = {newpets.gender}
-              description = {newpets.description}
-        
-              />
+      <h1>Animal Haven</h1>
+      <div className="pets ">
+        {pets.map((newpets) => {
+          return (
+            <div key={newpets.id}>
+              <div>
+                <DisplayPets
+                  name={newpets.name}
+                  age={newpets.age}
+                  species={newpets.species}
+                  breed={newpets.breed}
+                  image={newpets.image}
+                  gender={newpets.gender}
+                  description={newpets.description}
+                  id={newpets.id}
+                />
+              </div>
             </div>
-          </div>
-        )
-      })}
+          );
+        })}
+      </div>
     </div>
-    </div>
-   
   );
 }
